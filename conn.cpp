72fd27dc2850f2f8a8506a241ffe10423ff99644
ShuwenHe <1201220707@pku.edu.cn>
2024-11-08 20:37:13 +0800

    feat:shuwen

--- conn.cpp
+++ conn.cpp
@@ -6,12 +6,12 @@ int main(){
 	MYSQL_RES *res;
 	MYSQL_ROW row;
 	conn = mysql_init(NULL);
-	if(!mysql_real_connect(conn,"39.107.59.4","loan","Loan@_00..","loan",33061,NULL,0)){
+	if(!mysql_real_connect(conn,"39.107.59.4","shuwen","123456","shuwen",33061,NULL,0)){
 		std::cerr<<"error connecting to database:"<<mysql_error(conn)<<std::endl;
 		return 1;
 	}
 
-	if(mysql_query(conn,"SELECT id, username, password, phone, created_at, updated_at FROM loan.`user`;")){
+	if(mysql_query(conn,"SELECT id, username, password, phone, created_at, updated_at FROM shuwen.`user`;")){
 		std::cerr<<"error query database:"<<mysql_error(conn)<<std::endl;
 		return 1;
 	}
