2c56758f03abbddb695624dfd6567b899906cf71
ShuwenHe <1201220707@pku.edu.cn>
2024-11-09 17:22:03 +0800

    feat:json

--- user.cpp
+++ user.cpp
@@ -44,6 +44,8 @@ Json::Value get_user_data(){
 }
 
 int main(){
+	httplib::Server svr;
+	svr.Get("/getUserData",[](const httplib::Request& req,httplib::Response& res));
 	Json::Value data = get_user_data();
 	std::cout<<data.toStyledString()<<std::endl;
 
