ab7d94d6241e8a2a31af34439c4ca3d751491574
ShuwenHe <1201220707@pku.edu.cn>
2024-11-09 16:30:13 +0800

    feat:json

--- user.cpp
+++ user.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <mysql/mysql.h>
+#include <json/json.h>
 
 Json::Value get_user_data(){
-
-}
-
-int main(){
+	Json::Value json_data
 	MYSQL *conn;
 	MYSQL_RES *res;
 	MYSQL_ROW row;
+
 	conn = mysql_init(NULL);
-	if(!mysql_real_connect(conn,"39.107.59.4","shuwen","123456","shuwen",33061,NULL,0)){
+	if(!mysql_real_connect(conn,"39.107.59.4","shuwen","Shuwen@_00..","shuwen",33061,NULL,0)){
 		std::cerr<<"error connecting to database:"<<mysql_error(conn)<<std::endl;
 		return 1;
 	}
@@ -22,10 +21,30 @@ int main(){
 
 	res = mysql_use_result(conn);
 
-	std::cout<<"id\tusername\tpassword\tphone"<<std::endl;
+	Json::Value user(Json::arrayValue);
 	while((row = mysql_fetch_row(res)) != NULL){
-		std::cout<<row[0]<<"\t"<<row[1]<<"\t"<<row[2]<<"\t"<<row[3]<<std::endl;
+		Json::Value user;
+		user["id"] = row[0]?row[0]:"";
+		user["username"] = row[1]?row[1]:"";
+		user["password"] = row[2]?row[2]:"";
+		user["phone"] = row[3]?row[3]:"";
+		user["created_at"] = row[4]?row[4]:"";
+		user["updated_at"] = row[5]?row[5]:"";
+		user.append(user);
 	}
+	jsonData["status"] = "success";
+	jsonData["data"] = user;
 
+	mysql_free_result(res);
+	mysql_close(conn);
+
+	return json_data;
+}
+
+int main(){
+	Json::Value data = get_user_data();
+	std::cout<<data.toStyledString()<<std::endl;
+
+	return 0;
 }
 
